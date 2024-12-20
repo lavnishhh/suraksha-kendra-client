72c8c41cdd47c2bbbe1fc27bd80c9317143041e6
Karan Adam <karanadamv@gmail.com>
2024-12-20 12:27:03 +0530

    report map marker issue resolved

--- src/components/location_picker.jsx
+++ src/components/location_picker.jsx
@@ -1,7 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import { spinner } from "../constants/spinner";
+
+// Fix for missing markers in deployment
+const defaultIcon = L.icon({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+});
+
+L.Marker.prototype.options.icon = defaultIcon;
 
 const LocationPicker = ({ onPositionChange }) => {
     const [position, setPosition] = useState(null);
@@ -26,29 +41,27 @@ const LocationPicker = ({ onPositionChange }) => {
         });
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getUserLocation()
-        .then((location) => {
-            console.log("User's Location:", location);
-            setPosition({latitude: location.latitude, longitude: location.longitude})
-        })
-        .catch((error) => {
-            console.error("Error fetching location:", error);
-            setPosition({latitude: 12.980216112573299, longitude: 77.59734868843226})
-        });
-    }, [])
+            .then((location) => {
+                console.log("User's Location:", location);
+                setPosition({ latitude: location.latitude, longitude: location.longitude });
+            })
+            .catch((error) => {
+                console.error("Error fetching location:", error);
+                setPosition({ latitude: 12.980216112573299, longitude: 77.59734868843226 });
+            });
+    }, []);
 
-    useEffect(()=>{
-        onPositionChange(position)
-    }, [position])
-
+    useEffect(() => {
+        onPositionChange(position);
+    }, [position]);
 
     const LocationMarker = () => {
         useMapEvents({
             click(e) {
                 const { lat, lng } = e.latlng;
-                console.log(lat)
-                console.log(lng)
+                console.log(lat, lng);
                 setPosition({ latitude: lat, longitude: lng });
             },
         });
@@ -59,15 +72,21 @@ const LocationPicker = ({ onPositionChange }) => {
     return (
         <>
             <div className="my-3">
-                {position == null ? 
-                    <div className="flex">{spinner}<div className="ms-2">Fetching Location</div></div>:
-                    <div>{`${position.longitude} ${position.latitude}`}</div>} 
+                {position == null ? (
+                    <div className="flex">
+                        {spinner}
+                        <div className="ms-2">Fetching Location</div>
+                    </div>
+                ) : (
+                    <div>{`${position.longitude} ${position.latitude}`}</div>
+                )}
             </div>
-            <MapContainer center={[position? position.latitude: 12.980216112573299, position? position.longitude: 77.59734868843226]} zoom={10} style={{ height: "400px", width: "100%" }}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; OpenStreetMap contributors"
-                />
+            <MapContainer
+                center={[position ? position.latitude : 12.980216112573299, position ? position.longitude : 77.59734868843226]}
+                zoom={10}
+                style={{ height: "400px", width: "100%" }}
+            >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
                 <LocationMarker />
             </MapContainer>
         </>
